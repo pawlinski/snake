1cb80ed827cd836d4305433bad20492aae9a939d
rpawlinski <radoslaw.pawlinski@gmail.com>
2025-01-20 13:27:28 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import math
+import random
+import pygame
+import tkinter as tk
+from tkinter import messagebox
+
+# TODO: stwórz klasę -> kostka weża
+# TODO: stwórz klasę -> wąż
+# TODO: stwórz funkcję rysowania siatki
+# TODO: stwórz funkcję rysowania okna
+# TODO: stwórz funkcję rysowania jabłka
+# TODO: stwórz funkcję dla okna informacyjnego
+
+def main():
+    """main usuchamia się na początku gry/progrmu i jest odpowiedzialny za jego działanie, flow, etc."""
+    pass
+
+
+main()
